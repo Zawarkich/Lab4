cab482d4dd39ccc41317b1a81da632c6fcb1b222
Zawarkich <santotskyilya@gmail.com>
2025-06-16 14:49:25 +0300

    final cleanup

--- src/main/java/wikisearch/wiki_search/controller/GlobalExceptionHandler.java
+++ src/main/java/wikisearch/wiki_search/controller/GlobalExceptionHandler.java
@@ -13,21 +13,21 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleBadRequest(IllegalArgumentException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body("Ошибка 400: Некорректный запрос. " + ex.getMessage());
+                .body("Ошибка 400: Некорректный запрос.(Окак) " + ex.getMessage());
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleNotFound(ResourceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body("Ошибка 404: Ресурс не найден. " + ex.getMessage());
+                .body("Ошибка 404: Ресурс не найден.(Всё, потеряли) " + ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> handleInternalServerError(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Ошибка 500: Внутренняя ошибка сервера. " + ex.getMessage());
+                .body("Ошибка 500: Внутренняя ошибка сервера.(или создатель инвалид) " + ex.getMessage());
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
