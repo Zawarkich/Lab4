1b91ff7a0f20c5067c8e361b452b37a158e883fa
Zawarkich <santotskyilya@gmail.com>
2025-06-25 13:56:25 +0300

    Just deleted unneccesary files

--- src/main/java/wikisearch/wiki_search/controller/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package wikisearch.wiki_search.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
